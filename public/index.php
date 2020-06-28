8b1cf24c82b29a739656efdd0aba89881b1e9a46
sehee3527 <sehee3527@gmail.com>
2020-06-29 05:09:27 +0900

    3번째 배너 완성

--- public/index.php
+++ public/index.php
@@ -30,51 +30,62 @@
     <div class="bn-name">
         <div class="line-height-0 ">Web designer</div>
         <div>Sehee blog</div>
-         <div class="line-height-0 ">대충아무말나중에 쓰기</div>
+        <div class="line-height-0 ">대충아무말나중에 쓰기</div>
     </div>
 
 </div>
 
-<div class="fixed-img"><img src="https://images.unsplash.com/photo-1591563710505-7ebbc61ed8ce?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" alt=""></div>
+<div class="fixed-img"><img
+        src="https://images.unsplash.com/photo-1591563710505-7ebbc61ed8ce?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+        alt=""></div>
 
-    <div class="bn-2">
-        <div class="gray-circle1"></div>
-        <div class="gray-circle2"></div>
-        <div class="avatar">
-            <img src="/resource/img/avatar.png" alt="">
+<div class="bn-2">
+    <div class="gray-circle1"></div>
+    <div class="gray-circle2"></div>
+    <div class="avatar">
+        <img src="/resource/img/avatar.png" alt="">
+    </div>
+    <div class="profile-box">
+        <div>Web publisher & Web Designer</div>
+        <div>PROFILE</div>
+        <div>Yang Se hee</div>
+        <div class="gray-circle3"></div>
+    </div>
+    <div class="profile-move flex flex-jc-sb flex-ai-c">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div>자세히 보기</div>
+    </div>
+</div>
+<div class="bn-3">
+    <div class="yellow-circle">
+        <div class="bn-3-name flex flex-ai-c">
+            <div></div>
+            PORTFLIO
+            <div></div>
         </div>
-        <div class="profile-box">
-            <div>Web publisher & Web Designer</div>
-            <div>PROFILE</div>
-            <div>Yang Se hee</div>
-            <div class="gray-circle3"></div>
+        <div class="portflio-bn">
+            <div class="portflio-bn-in">
+                <div>WEB DESIGN</div>
+                <div class="portflio-bn-name">파나소닉</div>
+                <div class="portflio-bn-kind">-반응형 웹 사이트 <br>-모작 </div>
+            </div>
+
         </div>
-        <div class="profile-move flex flex-jc-sb flex-ai-c">
+        <div class="slider flex">
+            <div class="flex flex-ai-c flex-jc-c"><i class="fas fa-chevron-left"></i></div>
+            <div class="flex flex-ai-c flex-jc-c"><i class="fas fa-chevron-right"></i></div>
+        </div>
+        <div class="portflio-bn-content">
+            <img src="/resource/img/triangle-2.png" alt="">
+        </div>
+        <div class="portflio-view">
             <div></div>
-            <div></div>
-            <div></div>
-            <div>자세히 보기</div>
+            <a href="#">자세히 보기</a>
         </div>
     </div>
-    <div class="bn-3">
-        <div class="yellow-circle">
-            <div class="bn-3-name flex flex-ai-c">
-                <div></div>
-                PORTFLIO
-                <div></div>
-            </div>
-            <div class="portflio-bn">
-                <div class="portflio-bn-in">
-                    <div>WEB DESIGN</div>
-                    <div class="portflio-bn-name">파나소닉</div>
-                    <div class="portflio-bn-kind">-반응형 웹 사이트 <br>-모작 </div>
-                </div>
-            </div>
-            <div class="portflio-bn-content">
-                <img src="/resource/img/triangle-2.png" alt="">
-            </div>
-        </div>
-    </div>
+</div>
 <?php
     include "../part/foot.php";
 ?>
