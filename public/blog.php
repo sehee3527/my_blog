43a3e38bfa55e6abff12086e25f4575aaac99595
sehee3527 <sehee3527@gmail.com>
2020-07-02 18:37:15 +0900

    리스트 리디자인

--- public/blog.php
+++ public/blog.php
@@ -9,10 +9,52 @@
     </div>
 </div>
 <div class="blog-list con flex">
-    <div class="list-list"></div>
+    <nav class="blog-list-list">
+        <ul class="list-list flex flex-row-wrap">
+            <li>
+                <a></a>
+            </li>
+            <li>
+                <a>
+                    <div class="list-list-top-black"></div>
+                    <div class="line-1 line-1-1"></div>
+                    <div class="line-1 line-1-2"></div>
+                    <div class="line-2 line-2-1"></div>
+                    <div class="line-2 line-2-2">
+                        <div class="list-date">2020.07.02</div>
+                        <div class="list-name">제목</div>
+                        <div class="list-content">소제목 들어갈곳입니다.</div>
+                    </div>
+                </a>
+            </li>
+            <li>
+                <a></a>
+            </li>
+            <li>
+                <a></a>
+            </li>
+            <li>
+                <a></a>
+            </li>
+            <li>
+                <a></a>
+            </li>
+            <li>
+                <a></a>
+            </li>
+            <li>
+                <a></a>
+            </li>
+        </ul>
+    </nav>
     <div class="list-profile">
         <div class="list-profile-box">
             <img src="/resource/img/avatar.png" alt="">
+            <div class="list-profile-name">
+                Yang Sehee
+            </div>
+            <div class="list-profile-content">C_point 블로그에 오신것을 환영합니다</div>
+            <div>Welcome to the blog.</div>
         </div>
     </div>
 </div>
