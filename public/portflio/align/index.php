ecb331e96cdaf9a89e97d199135b78b858b53baa
sehee3527 <sehee3527@gmail.com>
2020-07-08 18:41:22 +0900

    포폴 메인배너1장짜리2개째 진행중

---
+++ public/portflio/align/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>align</title>
+    <link rel="stylesheet" href="align/index.css">
+</head>
+
+<body>
+<div class="top-box">
+    <div class="top-bar flex flex-ai-c con">
+        <div class="logo flex flex-ai-c flex-jc-c"><img src="http://sehee3527.github.io/img1/pf/align/logo-top.png"
+                alt=""></div>
+        <ul class="flex">
+            <li><a href="#">T9</a></li>
+            <li><a href="#">M9</a></li>
+            <li><a href="#">T9 loT</a></li>
+            <li><a href="#">UL</a></li>
+        </ul>
+    </div>
+</div>
+
+    <img src="http://sehee3527.github.io/img1/pf/align/main_20190214_1.jpg" alt="" class="main-img bn-1">
+    <div class="copy1">코콤이 만든 <span style="font-weight: 600">프리미엄</span> 브랜드 입니다.</div>
+    <div class="side-right-box">
+        <ul class="menu-fixed">
+            <li><a href="#">Align</a></li>
+            <li><a href="#">Movie</a></li>
+            <li><a href="#">First premium</a></li>
+            <li><a href="#">Vision & Misson</a></li>
+            <li><a href="#">Design</a></li>
+            <li><a href="#">Partnet</a></li>
+            <li><a href="#">Performance</a></li>
+            <li><a href="#">Align</a></li>
+        </ul>
+    </div>
+</body>
+
+</html>
---
+++ public/portflio/jeju-kakao/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>제주 with kakako</title>
+    <link rel="stylesheet" href="jeju-kakao/index.css">
+</head>
+
+<body>
+    <div class="top-bar-box">
+        <div class="top-bar flex flex-jc-sb con flex-ai-c">
+            <a href="#">
+                <span class="jeju">Jeju</span><span class="with-kakao">with kakao</span>
+            </a>
+            <ul class="flex flex-jc-sa">
+                <li><a href="#">공간소개</a></li>
+                <li><a href="#">With제주</a></li>
+                <li><a href="#">Live제주</a></li>
+                <li><a href="#">소식/보도자료</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="main-bn">
+        <img src="http://sehee3527.github.io/img1/pf/jeju-with-kakao/c582fb00e9f741df837ab262fdca28dc.png" alt="">
+    </div>
+</body>
+
+</html>
