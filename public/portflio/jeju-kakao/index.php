1aee15d7f5c86afaf0874dfe82dd544df4a88ccf
sehee3527 <sehee3527@gmail.com>
2020-07-09 18:37:46 +0900

    제주 with kakao

--- public/portflio/jeju-kakao/index.php
+++ public/portflio/jeju-kakao/index.php
@@ -12,8 +12,8 @@
 <body>
     <div class="top-bar-box">
         <div class="top-bar flex flex-jc-sb con flex-ai-c">
-            <a href="#">
-                <span class="jeju">Jeju</span><span class="with-kakao">with kakao</span>
+            <a href="#" class="logo">
+                <span class="jeju">Jeju</span><span class="with-kakao"> with kakao</span>
             </a>
             <ul class="flex flex-jc-sa">
                 <li><a href="#">공간소개</a></li>
@@ -23,9 +23,40 @@
             </ul>
         </div>
     </div>
-    <div class="main-bn">
-        <img src="http://sehee3527.github.io/img1/pf/jeju-with-kakao/c582fb00e9f741df837ab262fdca28dc.png" alt="">
+    <div class="mobile-top-bar-box flex flex-jc-sb flex-ai-c flex-1-0-0">
+        <div></div>
+        <div>Jeju<span> with kakao</span></div>
+        <div class="mobile-top-bar-menu flex flex-d-c flex-jc-sa">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
     </div>
+    <div class="main-bn">
+        <div class="main-bn-main-img" style="background-image:url('http://sehee3527.github.io/img1/pf/jeju-with-kakao/c582fb00e9f741df837ab262fdca28dc.png')"></div>
+        <div class="main-bn-content">
+            <div>더 좋은 제주를 위해 카카오가 응원합니다.</div>
+            <div>Jeju with <span>Kakao</span></div>
+            <div class="main-bn-bt flex flex-jc-c">
+
+                <div><a href="#">방문안내</a></div>
+                <div><a href="#">오시는 길</a></div>
+                <div><a href="#">하는 일</a></div>
+            </div>
+            <div class="main-bn-text">
+                <div>안녕하세요. Jeju with kakao 방문을 환영합니다!</div>
+                <div>제주와 카카오가 궁금하신가요? 'Jeju with kakao'를 통해, 카카오 스페이스 방문 정보와 유익한 지역 소식,</div>
+                <div>카카오가 제주의 '즐거운 변화'를 위해 진행하는 교류, 협력, 공헌 활동을 확인하실 수 있습니다.</div>
+                <a href="#">자세히 보기</a>
+            </div>
+            <div class="go-btm">
+                <div class="arrow"></div>
+            </div>
+
+        </div>
+    </div>
+
+
 </body>
 
 </html>
