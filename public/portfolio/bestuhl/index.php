ceea13952e054028cfcb3ea9e444b720d70d2d50
sehee3527 <sehee3527@gmail.com>
2020-07-13 18:42:27 +0900

    베스툴

---
+++ public/portfolio/bestuhl/index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>bestuhl</title>
+    <link rel="stylesheet" href="bestuhl/index.css">
+</head>
+
+<body>
+    <nav class="top-bar flex flex-jc-sb">
+        <ul class="menu-bar flex">
+            <li class="flex flex-ai-c"><a href="">회사소개</a>
+                <ul>
+                    <li>베스툴 스토리</li>
+                    <li>연혁&수상</li>
+                    <li>네트워크</li>
+                </ul>
+            </li>
+            <li class="flex flex-ai-c"><a href="">오피스</a>
+                <ul>
+                    <li class="md-namw">워크스테이션</li>
+                    <li>T1 DESK</li>
+                    <li>T-PANEL</li>
+                    <li>T-CABINET</li>
+                </ul>
+                <ul>
+                <li class="md-namw">회의/강의용 시스템</li>
+                    <li>F.A.T 10</li>
+                </ul>
+            </li>
+            <li class="flex flex-ai-c"><a href="">의자</a></li>
+            <li class="flex flex-ai-c"><a href="">프로젝트</a></li>
+            <li class="flex flex-ai-c"><a href="">고객서비스</a></li>
+            <li class="flex flex-ai-c"><a href="">뉴스</a></li>
+        </ul>
+        <ul class="top-right-bar flex flex-ai-c">
+            <li><a href="">ENG</a></li>
+            <li><a href="">SALES PORTAL</a></li>
+        </ul>
+        <div class="logo-box flex flex-ai-c">
+            <a href="/" class="logo block">
+                <img src="http://sehee3527.github.io/img1/pf/bestuhl/logo_white.png" alt="">
+                <img src="http://sehee3527.github.io/img1/pf/bestuhl/logo.png" alt="">
+            </a>
+        </div>
+        <div class="top-bar-bg"></div>
+    </nav>
+    </div>
+    <div class="main-bn">
+        <section class="main-bn-1">
+            <div class="main-bn-content">
+                <div>준비된 기술들이 한 자리에 있는 전혀 새로운 이야기</div>
+                <div>내 자리의 시작, 베스툴</div>
+                <div>MORE</div>
+            </div>
+        </section>
+        <section class="main-bn-2">
+            <div class="main-bn-content">
+                <div>업무 유형에 맞게 확장하는 솔루션</div>
+                <div>T1 DESK</div>
+                <div>MORE</div>
+            </div>
+        </section>
+        <section class="main-bn-3">
+            <div class="main-bn-content">
+                <div>실용적인 설계로 공간 활용도를 높인 공용 태블릿</div>
+                <div>M-TABLET</div>
+                <div>MORE</div>
+            </div>
+        </section>
+    </div>
+</body>
+
+</html>
--- public/portfolio/index.php
+++ public/portfolio/index.php
@@ -5,6 +5,8 @@
 <a href="/portfolio/panasonic">파나소닉</a><br>
 <a href="/portfolio/align">align</a><br>
 <a href="/portfolio/jeju-kakao">jeju-kakao</a>
+<a href="/portfolio/bestuhl">bestuhl</a>
+<a href="http://bestuhl.co.kr/">bestuhl홈페이지</a>
 
 <?php
     include "../../part/foot.php";
