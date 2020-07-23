daeb2d90a028e8a415c3120140578f61d1b66f0a
sehee3527 <sehee3527@gmail.com>
2020-07-23 18:47:58 +0900

    5번째 배너까지

--- public/portfolio/vavavillage/index.js
+++ public/portfolio/vavavillage/index.js
@@ -1,3 +1,6 @@
+var $window = $(window);
+var $html = $('html');
+
 function SlickSlider() {
     $(".slider-box > .slider").slick({
         dots: true,
@@ -15,11 +18,78 @@ function SlickSlider() {
         swipe: false,
         dots: false
     });
-    setTimeout(function() {
+    setTimeout(function () {
         $(".slider-box").addClass('ani-on');
     }, 10);
 }
 
+function ScrollSection__updateSection() {
+    setTimeout(function () {
+        $('[data-scroll-section]').each(function (index, el) {
+            // 날아다니는 물체
+            var $fo = $(el);
+            var $target = $($fo.attr('data-scroll-section'));
+
+            var topStart = $target.offset().top;
+            var topEnd = topStart + $target.height() - $fo.height();
+
+            $fo.data('data-scroll-section-top-start', topStart);
+            $fo.data('data-scroll-section-top-end', topEnd);
+
+            ScrollSection__updateTop($fo, $target);
+        });
+    }, 500);
+}
+
+function ScrollSection__updateTop($fo, $target) {
+    var topStart = $fo.data('data-scroll-section-top-start');
+    var topEnd = $fo.data('data-scroll-section-top-end');
+
+    console.log("topStart : " + topStart);
+    console.log("topEnd : " + topEnd);
+
+    var scrollTop = $window.scrollTop();
+
+    var newTop;
+
+    if (scrollTop < topStart) {
+        newTop = topStart;
+    } else if (scrollTop >= topStart && scrollTop <= topEnd) {
+        newTop = scrollTop;
+    } else {
+        newTop = topEnd;
+    }
+
+    $fo.css('top', newTop);
+}
+
+function ScrollSection__init() {
+    ScrollSection__updateSection();
+
+    $('[data-scroll-section]').each(function (index, el) {
+        // 날아다니는 물체
+        var $fo = $(el);
+        var $target = $($fo.attr('data-scroll-section'));
+
+        $window.scroll(function () {
+            ScrollSection__updateTop($fo, $target);
+        });
+    });
+
+    $(window).resize(ScrollSection__updateSection);
+}
+
+function scroller() {
+    $("#scroller").simplyScroll({
+        pauseOnHover: false,
+        speed: 2.5
+    });
+}
+
 $(function () {
     SlickSlider();
+    setTimeout(function () {
+        ScrollSection__init();
+    }, 1000);
+    scroller();
 });
--- public/portfolio/vavavillage/index.php
+++ public/portfolio/vavavillage/index.php
@@ -15,6 +15,11 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css">
+
+<!-- 심플리 스크롤 불러오기 -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-simplyscroll/2.1.1/jquery.simplyscroll.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-simplyscroll/2.1.1/jquery.simplyscroll.min.css">
+
     <script src="/portfolio/vavavillage/index.js"></script>
 
 
@@ -157,26 +162,143 @@
     <div class="bn-2 row">
         <div class="bn-2-main-img img-box cell"><a href="#" class="block img-box"><img
                     src="http://sehee3527.github.io/img1/pf/vavavillage/1329131e1c3e2b30317da305b2b48b8f_94055.png"
-                    alt=""><div class="bn-2-main-img-name">RUN TO <br>VAVA VILLATE<br>[CLICK]<br><div><span>당신의 일상에서 벗어나세요</span><span>바바빌리지는 당신을 </span><span>기다리고 있습니다.</span></div>
-    </div></a></div>
+                    alt="">
+                <div class="bn-2-main-img-name">RUN TO <br>VAVA VILLATE<br>[CLICK]<br>
+                    <div><span>당신의 일상에서 벗어나세요</span><span>바바빌리지는 당신을 </span><span>기다리고 있습니다.</span></div>
+                </div>
+            </a></div>
         <div class="bn-2-sub-img cell">
             <ul class="row">
                 <li class="cell"><a href="#" class="block img-box"><img
                             src="http://sehee3527.github.io/img1/pf/vavavillage/a4679a6768309d4e35e9a4bcc1809801_45143.jpg"
-                            alt=""><div class="bn-2-sub-img-name">BLANKET</div></a></li>
+                            alt="">
+                        <div class="bn-2-sub-img-name">BLANKET</div>
+                    </a></li>
                 <li class="cell"><a href="#" class="block img-box"><img
                             src="http://sehee3527.github.io/img1/pf/vavavillage/369f6cae3663c16692f9dea267b11174_13771.png"
-                            alt=""><div class="bn-2-sub-img-name">STICKER</div></a></li>
+                            alt="">
+                        <div class="bn-2-sub-img-name">STICKER</div>
+                    </a></li>
                 <li class="cell"><a href="#" class="block img-box"><img
                             src="http://sehee3527.github.io/img1/pf/vavavillage/e4562ff49d92d977a2fd57bbdc57034a_67446.png"
-                            alt=""><div class="bn-2-sub-img-name">VAVAPOSTER</div></a></li>
+                            alt="">
+                        <div class="bn-2-sub-img-name">VAVAPOSTER</div>
+                    </a></li>
                 <li class="cell"><a href="#" class="block img-box"><img
                             src="http://sehee3527.github.io/img1/pf/vavavillage/e35356260b919299c5b0da994963b350_94354.png"
-                            alt=""><div class="bn-2-sub-img-name">CLOTHES</div></a></li>
+                            alt="">
+                        <div class="bn-2-sub-img-name">CLOTHES</div>
+                    </a></li>
             </ul>
         </div>
     </div>
+    <!-- 배너4 -->
+    <div class="bn-4-best-item">
+        <div class="bn-4-name">BEST-ITEM</div>
+        <div class="bn-4-content flex">
+            <div class="bn-4-content-left">
+                <div class="bn-4-content-left-box follow-bar img-box" data-scroll-section=".bn-4-content-left">
+                    <img src="http://sehee3527.github.io/img1/pf/vavavillage/4691250cea4db6c3341fa36fed2caf7c_10825.png"
+                        alt="">
+                </div>
+            </div>
+            <div class="bn-4-content-right">
+                <ul class="best-item-box flex flex-row-wrap">
+                    <li class="best-item">
+                        <a href="#" class="block img-box"><img
+                                src="http://sehee3527.github.io/img1/pf/vavavillage/840f3d768a9410c4" alt=""></a>
+                        <div class="best-item-detail-box">
+                            <div class="best-item-detail">
+                                <a href="#" class="best-item-name">칼리크스펙트럼 x 바바빌리지 첫번째 콜라보</a>
+                                <div class="best-item-content">첫번째 콜라보 오버사이즈 후드티가 나왓습니다</div>
+                            </div>
+                            <a href="" class="best-item-product row">
+                                <div class="img-box cell">
+                                    <img src="http://sehee3527.github.io/img1/pf/vavavillage/1000000049_main_054.png"
+                                        alt="">
+                                </div>
+                                <div class="best-item-product-detail cell">
+                                    <div class="product-detail-name">바바빌리지 오버사이즈 XL 티셔츠</div>
+                                    <div class="product-detail-price">60.000원</div>
+                                </div>
+                            </a>
+                        </div>
+                    </li>
+                    <li class="best-item">
+                        <a href="#" class="block img-box"><img
+                                src="http://sehee3527.github.io/img1/pf/vavavillage/9495e7acfc759526" alt=""></a>
+                        <div class="best-item-detail-box">
+                            <div class="best-item-detail">
+                                <a href="#" class="best-item-name">칼리크스펙트럼 x 바바빌리지 두번째 콜라보</a>
+                                <div class="best-item-content">두번재 콜라보 심플티셔츠가 나왔습니다.</div>
+                            </div>
+                            <a href="" class="best-item-product row">
+                                <div class="img-box cell">
+                                    <img src="http://sehee3527.github.io/img1/pf/vavavillage/1000000049_main_054.png"
+                                        alt="">
+                                </div>
+                                <div class="best-item-product-detail cell">
+                                    <div class="product-detail-name">바바빌리지 티셔츠</div>
+                                    <div class="product-detail-price">35.000원</div>
+                                </div>
+                            </a>
+                        </div>
+                    </li>
+                    <li class="best-item">
+                        <a href="#" class="block img-box"><img
+                                src="http://sehee3527.github.io/img1/pf/vavavillage/316cf19b9f313e10" alt=""></a>
+                        <div class="best-item-detail-box">
+                            <div class="best-item-detail">
+                                <a href="#" class="best-item-name">바바갤러리</a>
+                                <div class="best-item-content">바바빌리지의 다양한 굿즈를 즐기실 수 있는 바바빌리지 갤러리를 준비 중 입니다.</div>
+                            </div>
+                            <a href="" class="best-item-product row">
+                                <div class="img-box cell">
+                                    <img src="http://sehee3527.github.io/img1/pf/vavavillage/1000000049_main_054.png"
+                                        alt="">
+                                </div>
+                                <div class="best-item-product-detail cell">
+                                    <div class="product-detail-name">바바빌리지 포스터</div>
+                                    <div class="product-detail-price">10.000원</div>
+                                </div>
+                            </a>
 
+                        </div>
+                    </li>
+                    <li class="best-item">
+                        <a href="#" class="block img-box"><img
+                                src="http://sehee3527.github.io/img1/pf/vavavillage/eabc9fc2f149e6f2" alt=""></a>
+                        <div class="best-item-detail-box">
+                            <div class="best-item-detail">
+                                <a href="#" class="best-item-name">바바빌리지 그랜드오픈</a>
+                                <div class="best-item-content">바바빌리지가 오픈기념으로 사계절 언제 어디서나 사용하실 수 잇는 바바빌리지 담요를 세일합니다.
+                                </div>
+                            </div>
+                            <a href="" class="best-item-product row">
+                                <div class="img-box cell">
+                                    <img src="http://sehee3527.github.io/img1/pf/vavavillage/1000000049_main_054.png"
+                                        alt="">
+                                </div>
+                                <div class="best-item-product-detail cell">
+                                    <div class="product-detail-name">바바빌리지 캐릭터담요</div>
+                                    <div class="product-detail-price">24.000원</div>
+                                </div>
+                            </a>
+                        </div>
+                    </li>
+                </ul>
+                <div class="more">
+                <a href="#" class="more-view">MORE-VIEW +</a>
+                </div>
+            </div>
+        </div>
+    </div>
+<!-- 5번째 배너 -->
+<div class="bn-5 scroll-text">
+    <div id="scroller">
+        <div class="text-scriller">VAVA VILLAGE <span>VAVA VILLAGE </span>VAVA VILLAGE <span>VAVA VILLAGE </span>VAVA VILLAGE </div>
+    </div>
+</div>
 </body>
 
 </html>
