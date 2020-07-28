ca238424b6c6fb3148bba478b052229b1b0fd7bb
sehee3527 <sehee3527@gmail.com>
2020-07-28 18:45:00 +0900

    아트파이브 영상수정

--- public/portfolio/artfive/index.js
+++ public/portfolio/artfive/index.js
@@ -1,17 +1,34 @@
-function FullPage(){    
-    
-    $(document).ready(function() {
-        $('#fullpage').fullpage({
-          scrollBar: true,
-          navigation: true,
-        //   슬라이드 직후에 실행
-          afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){
+function FullPage() {
 
-          }
-        });
+  $(document).ready(function () {
+    $('#fullpage').fullpage({
+      scrollingSpeed:500,
+      scrollBar: true,
+      navigation: true,
+      licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+      onLeave: function (origin, destination, direction) {
+        var $originVideo = $('#fullpage .section').eq(origin.index).find('video');
+        var $destinationVideo = $('#fullpage .section').eq(destination.index).find('video');
+
+        if ( $originVideo.length > 0 ) {
+          var video = $originVideo.get(0);
+          video.pause();
+        }
+
+        if ( $destinationVideo.length > 0 ) {
+          var video = $destinationVideo.get(0);
+          // video.currentTime = 0;
+          video.play();
+        };
+      }
+      //   슬라이드 직후에 실행
+      // afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){
+
+      // }
     });
+  });
 }
 
-$ (function() {
-    FullPage();
+$(function () {
+  FullPage();
 });
--- public/portfolio/artfive/index.php
+++ public/portfolio/artfive/index.php
@@ -1,12 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>모작 아트파이브</title>
-    <!-- 풀페이지 불러오기 -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <!-- 구글 폰트 불러오기 -->
+    <link
+        href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;700;900&family=Roboto:wght@400;700;900&display=swap"
+        rel="stylesheet">
+    <!-- 풀페이지 불러오기 -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/3.0.9/fullpage.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/3.0.9/fullpage.min.css">
 
@@ -14,18 +19,124 @@
     <link rel="stylesheet" href="/portfolio/artfive/index.css">
     <script src="/portfolio/artfive/index.js"></script>
 </head>
+
 <body>
     <div id="fullpage">
-        <div class="section bn"></div>
-        <div class="section bn"></div>
-        <div class="section bn"></div>
-        <div class="section bn"></div>
-        <div class="section bn"></div>
-        <div class="section bn"></div>
-        <div class="section bn"></div>
-        <div class="section bn"></div>
-        <div class="section bn"></div>
-        <div class="section bn"></div>
+        <div class="section bn1">
+            <div class="bn-bg">
+                <video width="100%" muted="muted" autoplay loop>
+                    <source src="https://sehee3527.github.io/img1/pf/artfive/v005.mp4" type="video/mp4">
+                </video>
+                <div class="text-box">
+                    <div>신개념 프리미엄 어린이 미술교육</div>
+                    <div>다섯손가락의 기적</div>
+                    <div>Creative Kids Power</div>
+                </div>
+                <div class="scroll-down">
+                    <div class="line"></div>
+                    <div class="scroll-bt">Scroll down</div>
+                </div>
+            </div>
+        </div>
+        <div class="section bn2">
+            <div class="bn-bg img-box">
+                <img src="http://sehee3527.github.io/img1/pf/artfive/v02.jpg" alt="">
+
+                <div class="text-box img-box">
+                    <div>창의성, 세계의 확장</div>
+                    <div>지식 습득에 익숙한 아이는 과거지향적입니다.<br>
+                        정답을 찾기 위해 노력하고, 그 안에서 만족합니다.<br>
+                        탁월한 아이는 미지의 것이 주는 불안감을 감당하면서<br>
+                        새로운 삶의 영역을 확장시킵니다.</div>
+                </div>
+                <div class="scroll-down">
+                    <div class="line"></div>
+                    <div class="scroll-bt">Scroll down</div>
+                </div>
+            </div>
+        </div>
+        <div class="section bn3">
+            <div class="bn-bg img-box">
+                <img src="http://sehee3527.github.io/img1/pf/artfive/v03.jpg" alt="">
+                <div class="text-box">
+                    <div>질문하는 아이<br>
+                        대답하는 아이</div>
+                    <div>대답 하는 아이는 이론과 지식이 머물다 가는 통로로서 존재합니다.<br>
+                        그러나 자신만의 고유한 시각, 감성을 가진 아이는<br>
+                        예측치 못한 상황을 접할 때 호기심을 갖게됩니다.</div>
+                </div>
+                <div class="scroll-down">
+                    <div class="line"></div>
+                    <div class="scroll-bt">Scroll down</div>
+                </div>
+            </div>
+        </div>
+        <div class="section bn4">
+            <div class="bn-bg img-box">
+                <img src="http://sehee3527.github.io/img1/pf/artfive/v04.jpg" alt="">
+                <div class="text-box">
+                    <div>3살, 세상의 모든 것을<br>
+                        배우기에 충분한 나이</div>
+                    <div>아이의 뇌는 성장함에 있어 결정적 시기가 있습니다.<br>
+                        뇌의 시냅스가 얼만큼 긴밀하고 촘촘하게 연결되는가 하는것이<br>
+                        우리 아이가 밟아 갈 세상의 크기를 결정합니다.</div>
+                </div>
+                <div class="scroll-down">
+                    <div class="line"></div>
+                    <div class="scroll-bt">Scroll down</div>
+                </div>
+            </div>
+        </div>
+        <div class="section bn5">
+            <div class="bn-bg img-box">
+                <img src="http://sehee3527.github.io/img1/pf/artfive/v05.jpg" alt="">
+                <div class="text-box">
+                    <div>놀이, 진짜와 가짜</div>
+                    <div>몰입과 경험이 아이의 내적 동기에 의해서 발생할때,<br>
+                        놀이는 효과를 발휘하게 됩니다.<br>
+                        통제하고 가르치는 놀이는 아이의 무한한 잠재력을 깨우지 못하고<br>
+                        통제된 범위 속에서만 머무르게 합니다.</div>
+                </div>
+                <div class="scroll-down">
+                    <div class="line"></div>
+                    <div class="scroll-bt">Scroll down</div>
+                </div>
+            </div>
+        </div>
+        <div class="section bn6">
+            <div class="bn-bg">
+                <video width="100%" muted="muted" autoplay loop>
+                    <source src="https://sehee3527.github.io/img1/pf/artfive/v003.mp4" type="video/mp4">
+                </video>
+                <div class="text-box">
+                    <div>예술, 최고의 나를 만나는 순간</div>
+                    <div>天人合一, 物我一體<br>
+                        대상과 내 자신이 하나가 되는 순간<br>
+                        아이 스스로 자신의 존재를 인식하게 됩니다.<br>
+                        자존감과 정체성은 유아동기의 미술 교육이 주는 가치입니다.</div>
+                </div>
+                <div class="scroll-down">
+                    <div class="line"></div>
+                    <div class="scroll-bt">Scroll down</div>
+                </div>
+            </div>
+        </div>
+        <div class="section bn7">
+            <div class="bn-bg center">
+                <div class="bn7-name">
+                    ARTFIVE MERIT
+                </div>
+                <div class="bn7-name-content">
+
+                </div>
+            </div>
+
+        </div>
+        <div class="section bn8">
+        </div>
+        <div class="section bn9"></div>
+        <div class="section bn10"></div>
     </div>
 </body>
+
 </html>
