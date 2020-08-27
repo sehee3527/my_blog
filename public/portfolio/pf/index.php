2a7898e51cbf1f86954e1851d83817e3234a5ff9
sehee3527 <sehee3527@gmail.com>
2020-08-27 18:41:40 +0900

    포폴 코딩중

--- public/portfolio/pf/index.php
+++ public/portfolio/pf/index.php
@@ -1,21 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>포폴 리디자인</title>
-    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;700;900&family=Roboto:wght@400;700;900&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;700;900&family=Roboto:wght@400;700;900&display=swap"
+        rel="stylesheet">
     <link rel="stylesheet" href="/portfolio/pf/index.css">
     <link rel="stylesheet" href="/resource/font/font.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="/portfolio/pf/index.js"></script>
 </head>
+
 <body>
     <section class="page-1">
         <div class="page-1-con con">
             <div>PORTFOLIO</div>
             <div>c.point</div>
-            <div><div class="img-box"><img src="http://sehee3527.github.io/img1/pf/pf/main-rotate.png" alt=""></div></div>
+            <div>
+                <div class="img-box"><img src="http://sehee3527.github.io/img1/pf/pf/main-rotate.png" alt=""></div>
+            </div>
             <div>
                 <div></div>
                 <div></div>
@@ -36,12 +42,156 @@
                 <div></div>
             </div>
             <div>
-            <div class="page-2-content">
-                <div>안녕하세요</div>
-                <div>대충 아무말 쓰기대충 아무말 쓰기대충 아무말 쓰기대충 아무말 쓰기대충 아무말 쓰기대충 아무말 쓰기대충 아무말 쓰기대충 아무말 쓰기대충 아무말 쓰기대충 아무말 쓰기</div>
+                <div class="page-2-content">
+                    <div>반갑습니다</div>
+                    <div>함께 발전해나가고 싶은 웹 퍼블리셔 양세희 입니다.</div>
+                </div>
             </div>
+
+        </div>
+    </section>
+    <section class="page-3">
+        <div class="pad img-box"><img src="http://sehee3527.github.io/img1/pf/pf/pad_1.png" alt="">
+            <div class="pad-content">
+                <div class="pad-top-bar flex flex-jc-sa">
+                    <div></div>
+                    <div class="flex-grow-1">profile</div>
+                    <div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                </div>
+                <div class="pad-in flex">
+                    <div class="information">
+                        <div>INFORMATION</div>
+                        <div><img src="" alt=""></div>
+                        <div class="info-text flex">
+                            <div>
+                                NAME<br>
+                                BIRTH<br>
+                                FHONE<br>
+                                EMAIL<br>
+                                ADDRESS<br>
+                                <div>
+                                    BLOG<br>
+                                    WIKI<br>
+                                    EDUCATION<br>
+                                </div>
+                            </div>
+                            <div>
+                                양세희<br>
+                                1997.03.08<br>
+                                010-6630-9627<br>
+                                sehee3527@gmail.com<br>
+                                대전광역시 서구<br>
+                                <div>
+                                    https://sehee35271.tistory.com/<br>
+                                    https://to2/kr/bfB<br>
+                                    대전 SBS아카데미 컴퓨터 아트 학원<br>웹과정 수료
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="skill flex-grow-1">
+                        <div>SKILL</div>
+                        <div class="skills flex flex-row-wrap">
+                            <div class="flex">
+                                <div>
+                                    <div>
+                                        <div class="img-box"><img
+                                                src="http://sehee3527.github.io/img1/pf/pf/skill-01.png" alt="">
+                                                <div>HTML</div>
+                                            </div>
+                                    </div>
+                                </div>
+                                <div>
+                                    <div>기본기 탄탄한 의미있는 구조 마크업,
+                                        웹 표준·웹 접근성에 대한 전반적인 이해</div>
+                                    <div>70%</div>
+                                </div>
+                            </div>
+                            <div class="flex">
+                                <div>
+                                    <div>
+                                        <div class="img-box"><img
+                                                src="http://sehee3527.github.io/img1/pf/pf/skill-01.png" alt="">
+                                                <div>CSS</div>
+                                            </div>
+                                    </div>
+                                </div>
+                                <div>
+                                    <div>기본기 탄탄한 의미있는 구조 마크업,
+                                        웹 표준·웹 접근성에 대한 전반적인 이해</div>
+                                    <div>70%</div>
+                                </div>
+                            </div>
+                            <div class="flex">
+                                <div>
+                                    <div>
+                                        <div class="img-box"><img
+                                                src="http://sehee3527.github.io/img1/pf/pf/skill-01.png" alt="">
+                                                <div>JQ</div>
+                                            </div>
+                                    </div>
+                                </div>
+                                <div>
+                                    <div>기본기 탄탄한 의미있는 구조 마크업,
+                                        웹 표준·웹 접근성에 대한 전반적인 이해</div>
+                                    <div>70%</div>
+                                </div>
+                            </div>
+                            <div class="flex">
+                                <div>
+                                    <div>
+                                        <div class="img-box"><img
+                                                src="http://sehee3527.github.io/img1/pf/pf/skill-01.png" alt="">
+                                                <div>JS</div>
+                                            </div>
+                                    </div>
+                                </div>
+                                <div>
+                                    <div>기본기 탄탄한 의미있는 구조 마크업,
+                                        웹 표준·웹 접근성에 대한 전반적인 이해</div>
+                                    <div>70%</div>
+                                </div>
+                            </div>
+                            <div class="flex">
+                                <div>
+                                    <div>
+                                        <div class="img-box"><img
+                                                src="http://sehee3527.github.io/img1/pf/pf/skill-01.png" alt="">
+                                                <div>PS</div>
+                                            </div>
+                                    </div>
+                                </div>
+                                <div>
+                                    <div>기본기 탄탄한 의미있는 구조 마크업,
+                                        웹 표준·웹 접근성에 대한 전반적인 이해</div>
+                                    <div>70%</div>
+                                </div>
+                            </div>
+                            <div class="flex">
+                                <div>
+                                    <div>
+                                        <div class="img-box"><img
+                                                src="http://sehee3527.github.io/img1/pf/pf/skill-01.png" alt="">
+                                                <div>AI</div>
+                                            </div>
+                                    </div>
+                                </div>
+                                <div>
+                                    <div>기본기 탄탄한 의미있는 구조 마크업,
+                                        웹 표준·웹 접근성에 대한 전반적인 이해</div>
+                                    <div>70%</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
 </body>
+
 </html>
