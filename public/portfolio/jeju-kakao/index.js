233bb5e4e8d752dd41aea4b22bf37c1ee1a06128
sehee3527 <sehee3527@gmail.com>
2020-10-12 23:53:51 +0900

    제주 카카오 끝

--- public/portfolio/jeju-kakao/index.js
+++ public/portfolio/jeju-kakao/index.js
@@ -1,16 +1,36 @@
 function mobileTopBarMenuClick() {
     var $mobileTopBarBox = $('.mobile-top-bar-box');
 
-    $('.mobile-top-bar-box > .mobile-top-bar-menu').click(function() {
-        if ($mobileTopBarBox.hasClass('active')){
+    $('.mobile-top-bar-box > .mobile-top-bar-menu').click(function () {
+        if ($mobileTopBarBox.hasClass('active')) {
             $mobileTopBarBox.removeClass('active')
-        }
-        else{
+        } else {
             $mobileTopBarBox.addClass('active')
         }
     });
 };
 
+function footerButton() {
+    var $footerBtton = $('#footer>ul>li:last-child>ul')
+    $('#footer>ul>li:last-child>button').click(function () {
+        if ($footerBtton.hasClass('active')) {
+            $footerBtton.removeClass('active')
+        } else {
+            $footerBtton.addClass('active')
+        }
+    })
+}
+$(document).ready(function () {
+    var swiper = new Swiper('.swiper-container', {
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+});
+
+
 $(function () {
+    footerButton();
     mobileTopBarMenuClick();
 });
--- public/portfolio/jeju-kakao/index.php
+++ public/portfolio/jeju-kakao/index.php
@@ -6,9 +6,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>제주 with kakako</title>
+    <link rel="icon" href="https://t1.kakaocdn.net/jewika/images/favicon.ico">
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <!-- swiper 불러오기 -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/5.4.5/js/swiper.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/5.4.5/css/swiper.min.css">
+
     <link rel="stylesheet" href="/portfolio/jeju-kakao/index.css">
     <link rel="stylesheet" href="/portfolio/jeju-kakao/common.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="/portfolio/jeju-kakao/index.js"></script>
 </head>
 
@@ -81,33 +88,112 @@
         </div>
     </div>
 
-    <div id="today"></div>
+    <div id="today">
+        <div class="today-top con">
+            Today
+        </div>
+        <div class="today-content con flex flex-jc-sb">
+            <div class="weather flex flex-ai-c">
+                <div class="img-box"><img src="http://sehee3527.github.io/img1/pf/jeju-with-kakao/weather_sun_3.png"
+                        alt=""></div>
+                <div>15<span>°C</span></div>
+                <div>18시 현재, 구름조금<br>미세먼지<span>보통</span> </div>
+            </div>
+            <div class="calender">
+                <div><span>10월 카카오 스페이스 쉬는날</span>아래 일정을 확인하시고 방문해 주세요~!</div>
+                <div>
+
+                    <!-- Swiper -->
+                    <div class="swiper-container">
+                        <div class="swiper-wrapper">
+                            <div class="swiper-slide">
+                                <ul class="flex flex-jc-sa">
+                                    <li class="day">28</li>
+                                    <li class="day">29</li>
+                                    <li class="day">30</li>
+                                    <li>1</li>
+                                    <li>2</li>
+                                    <li>3</li>
+                                    <li>4</li>
+                                </ul>
+                            </div>
+                            <div class="swiper-slide">
+                                <ul class="flex flex-jc-sa">
+                                    <li>5</li>
+                                    <li>6</li>
+                                    <li>7</li>
+                                    <li>8</li>
+                                    <li>9</li>
+                                    <li>10</li>
+                                    <li>11</li>
+                                </ul>
+                            </div>
+                            <div class="swiper-slide">
+                                <ul class="flex flex-jc-sa">
+                                    <li>12</li>
+                                    <li>13</li>
+                                    <li>14</li>
+                                    <li>15</li>
+                                    <li>16</li>
+                                    <li>17</li>
+                                    <li>18</li>
+                                </ul>
+                            </div>
+                            <div class="swiper-slide">
+                                <ul class="flex flex-jc-sa">
+                                    <li>19</li>
+                                    <li>20</li>
+                                    <li>21</li>
+                                    <li>22</li>
+                                    <li>23</li>
+                                    <li>24</li>
+                                    <li>25</li>
+                                </ul>
+                            </div>
+                            <div class="swiper-slide">
+                                <ul class="flex flex-jc-sa">
+                                    <li>26</li>
+                                    <li>27</li>
+                                    <li>28</li>
+                                    <li>29</li>
+                                    <li>30</li>
+                                    <li>31</li>
+                                    <li>1</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <!-- Add Arrows -->
+                        <div class="swiper-button-next"></div>
+                        <div class="swiper-button-prev"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <div id="live-jeju" class="con">
-        <div class="live-jeju-top flex">
+        <div class="live-jeju-top flex flex-jc-sb">
             <div>Live 제주</div>
-            <img src="" alt="">
+            <div class="arrow">
+            </div>
         </div>
         <ul class="flex">
             <li>
-                <div class="img-box">
-                    <img src="http://sehee3527.github.io/img1/pf/jeju-with-kakao/다운로드 (1).jpg" alt="">
+                <div class="img-box" style="background-image:url(http://sehee3527.github.io/img1/pf/jeju-with-kakao/다운로드(1).jpg)">
                 </div>
-                <div class="live-jeju-name">면역력에 도음 되는 본투비 제주 제품</div>
+                <div class="live-jeju-name">면역력에 도움 되는 본투비 제주 제품</div>
                 <div class="live-jeju-content">안녕하세요, 여러분! 어두운 시기 속에서도 봄이 왔습니다. 모두 각자의 방법으로 봄을 만끽하고 계실텐데요 이럴 때일수록 건강을 더욱
                     잘 챙경 한다는 스아실~ 일교차가 심한 이...</div>
             </li>
             <li>
-                <div class="img-box">
-                    <img src="http://sehee3527.github.io/img1/pf/jeju-with-kakao/다운로드.jpg" alt="">
+                <div class="img-box" style="background-image:url(http://sehee3527.github.io/img1/pf/jeju-with-kakao/다운로드.jpg)">
                 </div>
                 <div class="live-jeju-name">방구석에서 편하게 즐길 수 있는 제주 온라인 전시</div>
                 <div class="live-jeju-content">안녕하세요, 여러분! 끝날듯 말듯한 코로나19의 여파로 많이 답답한 상황을 겪고 계실텐데요..! 여기, 현 시기를 위로하듯이 등장한
                     제주 온라인 전시가 있습니다. 장소와 상황의...</div>
             </li>
             <li>
-                <div class="img-box">
-                    <img src="http://sehee3527.github.io/img1/pf/jeju-with-kakao/다운로드.png" alt="">
+                <div class="img-box" style="background-image:url(http://sehee3527.github.io/img1/pf/jeju-with-kakao/다운로드.png)">
                 </div>
                 <div class="live-jeju-name">집에서 편하고 쉽게 즐길 수 있는 제주 산지 식품</div>
                 <div class="live-jeju-content">인사말 = 밥 먹였샤? 속상할 때 밥맛 쩔어져 감사인사 = 밥 살게 무언가를 잘 해야 할 때 = 밥값은 해야지 성공할 일을 망쳤을
@@ -116,13 +202,137 @@
         </ul>
     </div>
 
-    <div id="in-kakao-space"></div>
+    <div id="in-kakao-space">
+        <div class="in-kakao-space-top flex flex-jc-sb con">
+            <div>in Kakao Space</div>
+            <div class="arrow"></div>
+        </div>
+        <div class="in-kakao-space-content con">
+            <div>
+                <strong>Travel Day in jeju(잠정중단)</strong>
+                <p>코로나19로 인하여 Trav3el Day 운영을 잠정 중단하게 되었습니다.<br>트래블러여러분의 양해를 부탁드립니다.</p>
+                <a href="#" class="btn">자세히 보기</a>
 
-    <div id="notice"></div>
+            </div>
+        </div>
+    </div>
 
-    <div id="with-jeju"></div>
+    <div id="notice">
+        <div class="notice-top con flex flex-jc-sb">
+            <div>소식 · 보도자료</div>
+            <div></div>
+        </div>
+        <ul class="notice-content con flex flex-jc-sb">
+            <li>
+                <strong class="name">[안내]코로나로 인한 Travel Day 잠정중단</strong>
+                <p class="content">코로나 19확산 방지와 안전 확보를 위해, Travel Day 방문 프로그램을 포함한 카카오 스페이스 닷원 시설 운영을 잠정 중단하고 있습니다.
+                    프래블러 여러분의 양해를 부탁드립니...</p>
+            </li>
+            <li>
+                <strong class="name">[모집 마감] Kakao 클래스 - 광고마케팅, 톡스토어 클래스 (심화과정)</strong>
+                <p class="content">지난 8월 광고마케팅 / 톡스토어 만들기 기본 과정에 이어 소상공인, 스타트업, 사업자들을 대상으로 심화과정이 진행됩니다. 1. '카카오 플랫폼을 활용한
+                    광고 마케팅' 심화과정 *프...</p>
+            </li>
 
-    <div id="footer"></div>
+            <li>
+                <strong class="name">[모집 마감] 2019 언플러그드데이 참가자 모집합니다.</strong>
+                <p class="content">[2019 언플러그드데이]-일시 ㅣ:2019년 9월 21일 (토요일)오후 2시~5시 -장소: 카카오 스페이스닷원 (제주시 첨단로242)-모집 인원
+                    :제주도내 재학중인 초등학...</p>
+            </li>
+        </ul>
+    </div>
+
+    <div id="with-jeju">
+        <div class="with-jeju-name con flex">
+            <div>With 제주</div>
+            <div></div>
+        </div>
+        <ul class="with-jeju-content con row">
+            <li style="background-image:url(http://sehee3527.github.io/img1/pf/jeju-with-kakao/thumb_withjeju01.jpg)"
+                class="cell">
+                <div class="text-box">
+                    <div class="name">청소년 IT교육</div>
+                    <div class="content">지식정보사업 환경은 멈춤없이 변하고 있습니다.<br>카카오는 이런 미래에 제주 청소년들이 능동적으로 대처할 수 있도록<br>진로체험, 코딩교실
+                        등 다양한 IT교육 프로그램을 개발/운영하고 있습니다.</div>
+                    <div class="btn"><a href="">자세히보기</a></div>
+                </div>
+            </li>
+            <li style="background-image:url(http://sehee3527.github.io/img1/pf/jeju-with-kakao/thumb_withjeju02.jpg)"
+                class="cell">
+                <div class="text-box">
+                    <div class="name">Kakao 클래스</div>
+                    <div class="content">제주도 내 예비창업자, 소상공인, 창작자 스타트업을 대상으로 하며,<br>카카오 서비스 플랫폼 활용 노하우를 배워보는<br>강의프로그램
+                        입니다.</div>
+                    <div class="btn"><a href="">자세히보기</a></div>
+                </div>
+            </li>
+            <li style="background-image:url(http://sehee3527.github.io/img1/pf/jeju-with-kakao/thumb_withjeju03.jpg)"
+                class="cell">
+                <div class="text-box">
+                    <div class="name">지역협력</div>
+                    <div class="content">카카오는 VR,빅테이터, 지역혁신 분야에 대해 도내 관/기관 및 기업과 협력하고 있습니다.<br>더 좋은 제주를 위한 카카오의 노력을
+                        소개합니다.</div>
+                    <div class="btn"><a href="">자세히보기</a></div>
+                </div>
+            </li>
+            <li style="background-image:url(http://sehee3527.github.io/img1/pf/jeju-with-kakao/thumb_withjeju04.jpg)"
+                class="cell">
+                <div class="text-box">
+                    <div class="name">인터넷 하는 돌하르방</div>
+                    <div class="content">제주와 제주 이웃을 즐겁게 변화시키는 착한 소원을 듣고<br>도움을 주는 카카오의 사화 공헌활동입니다.<br>카카오스페이스를 방문하기면
+                        직접만나볼 수도 있답니다.</div>
+                    <div class="btn"><a href="">자세히보기</a></div>
+                </div>
+            </li>
+        </ul>
+    </div>
+
+    <div id="footer" style="background-image:url(https://t1.kakaocdn.net/jewika/images/footer_bg.png)">
+        <ul class="con flex felx-jc-sb">
+            <li>
+                <div>
+                    Capyright ⓒ Kakao Corp.All rights reserved.<br>제주특별자치도 제주시 첨단로 242<br>제안/문의 jeju@kakaocorp.com
+                </div>
+                <div class="flex flex-ai-c">
+                    <img src="https://t1.kakaocdn.net/jewika/images/v1/icon_plusfriend.png" alt="">
+                    <span>플러스 친구 추가하기</span>
+                </div>
+            </li>
+            <li class="flex flex-grow-1 ">
+                <ul>
+                    <li>공간소개</li>
+                    <li><a href="">카카오스페이스</a></li>
+                    <li><a href="">지도/주요장소</a></li>
+                    <li><a href="">방문안내</a></li>
+                    <li><a href="">오시는 길</a></li>
+                </ul>
+                <ul>
+                    <li>With제주</li>
+                    <li><a href="">교육협력</a></li>
+                    <li><a href="">지역협력</a></li>
+                    <li><a href="">교류 및 공헌</a></li>
+                </ul>
+                <ul>
+                    <li>Live제주</li>
+                </ul>
+                <ul>
+                    <li>소식/보도자료</li>
+                </ul>
+            </li>
+            <li class="flex">
+                <div>
+                    관련사이트
+                </div>
+                <button name="button">+</button>
+                <ul>
+                    <li>카카오</li>
+                    <li>동반성장 with kakao</li>
+                    <li>같이가치 with kakao</li>
+                </ul>
+            </li>
+
+        </ul>
+    </div>
 
 </body>
 
